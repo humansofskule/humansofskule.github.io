f1afe84b0e9a8e4dac7879813a3f472a2e207372
PrernaAnand <prerna.anand@mail.utoronto.ca>
2021-01-18 22:41:42 -0500

    Fixed: email icon, nominate and join us

--- src/App.js
+++ src/App.js
@@ -8,8 +8,6 @@ import Home from './components/home';
 import Gallery from './components/gallery';
 import Video from './components/video';
 import Workshop from './components/workshop';
-import Nominate from './components/nominate';
-import JoinUs from './components/joinus';
 import Year1 from "./components/year1";
 import Year2 from "./components/year2";
 import Year3 from "./components/year3";
@@ -29,8 +27,6 @@ class App extends Component {
                         <Route path="/gallery" component={Gallery}/>
                         <Route path="/video" component={Video}/>
                         <Route path="/workshop" component={Workshop}/>
-                        <Route path="/nominate" component={Nominate}/>
-                        <Route path="/joinus" component={JoinUs}/>
                         <Route path="/year1" component={Year1}/>
                         <Route path="/year2" component={Year2}/>
                         <Route path="/year3" component={Year3}/>
--- src/components/navigation.js
+++ src/components/navigation.js
@@ -8,6 +8,7 @@ import {
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { Nav, Navbar } from 'react-bootstrap';
 import styled from 'styled-components';
+import * as Icon from 'react-bootstrap-icons';
 const Styles = styled.div`
   .navbar { background-color: black; }
   .navbar-nav, .navbar-light .nav-link {
@@ -17,6 +18,8 @@ const Styles = styled.div`
   .navbar-light .navbar-toggler {
       background-color: white;
   }
+  .nav-link.active {
+    color: white !important; 
 `;
                    
 function Navigation() {
@@ -33,10 +36,11 @@ function Navigation() {
                     <Nav.Item><Nav.Link href="/gallery">Picture Gallery</Nav.Link></Nav.Item>
                     <Nav.Item><Nav.Link href="/video">Video Gallery</Nav.Link></Nav.Item>
                     <Nav.Item><Nav.Link href="/workshop">Workshop</Nav.Link></Nav.Item>
-                    <Nav.Item><Nav.Link href="/nominate">Nominate</Nav.Link></Nav.Item>
-                    <Nav.Item><Nav.Link href="/joinus">Join Us</Nav.Link></Nav.Item>
                 </Nav>
                 <Nav className="ml-auto">
+                    <Nav.Item><Nav.Link href="https://docs.google.com/forms/d/e/1FAIpQLScgQH6Y4md389fMffH7K1i4KZzS2I9YjEyM1Zbn2ny5-2ZUvA/viewform" target='blank'>Nominate</Nav.Link></Nav.Item>
+                    <Nav.Item><Nav.Link href="https://docs.google.com/forms/d/e/1FAIpQLSc9PlkyeDRJ1ftnPeX8Rrnp9N2xLtuldnInkJSEizRLJMP-4Q/viewform?usp=sf_link" target='blank'>Join Us</Nav.Link></Nav.Item>
+                    <Nav.Item><Nav.Link href="mailto:humansofskule@gmail.com" target='blank'><Icon.EnvelopeFill  style={{marginTop:'-3px'}}/></Nav.Link></Nav.Item>
                     <Nav.Item><Nav.Link href="https://www.facebook.com/humans.of.skule" target='blank'><FontAwesomeIcon icon={faFacebook} style={{color:'white'}} size='1.5x'/></Nav.Link></Nav.Item>
                     <Nav.Item><Nav.Link href="https://www.instagram.com/humans.of.skule/" target='blank'><FontAwesomeIcon icon={faInstagram} style={{color:'white'}} size='1.5x'/></Nav.Link></Nav.Item>
                     <Nav.Item><Nav.Link href="https://twitter.com/HumansofSkule" target='blank'><FontAwesomeIcon icon={faTwitter} style={{color:'white'}} size='1.5x'/></Nav.Link></Nav.Item>
